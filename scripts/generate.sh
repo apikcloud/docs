b99f4a44dd20b2eed0ef4c068aa9ba064d08ab5b
Aurelien ROY <roy.aurelien@gmail.com>
2025-11-26 19:17:57 +0100

    fix: inline header file path in script [skip ci]

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -8,10 +8,9 @@ fi
 
 INPUT_FILE="$1"
 OUTPUT_FILE="$2"
-HEADER_FILE="assets/header.tex"
-
-pandoc --listings \                                                                                                                                    
-    -H "$HEADER_FILE" \
+  
+pandoc --listings \
+    -H assets/header.tex \
     -V geometry:"left=2.5cm, top=2.5cm, right=2.5cm, bottom=2.5cm" \
     -V fontsize=11pt \
     -V mainfont="DejaVu Serif" \
@@ -19,4 +18,4 @@ pandoc --listings \
     "$INPUT_FILE" \
     --from=gfm \
     --pdf-engine=xelatex \
-    -o "$OUTPUT_FILE"    
+    -o "$OUTPUT_FILE"
